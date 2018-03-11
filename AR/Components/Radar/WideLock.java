67870b0bc52af1e2a6344c943c0d93a68fb0c69c
Rodrigo Marques <rodrigocanedomarques@hotmail.com>
2018-03-11 03:07:59 +0000

    Add files via upload

--- AR/Components/Radar/WideLock.java
+++ AR/Components/Radar/WideLock.java
@@ -1,33 +1,33 @@
 package AR.Components.Radar;
 
+import robocode.AdvancedRobot;
 import robocode.ScannedRobotEvent;
 import robocode.util.Utils;
 
-import AR.Beck;
 import AR.Components.Component;
 
-/*
-  Becks's Radar
-  Simple Wide Lock implementation.
-  We turn PI/4 radians if we don't have a target,
-  PI/8 if we do.
-
+/**
+ * Simple Wide Lock implementation for our robot
+ * Turn the radar PI/4 radians while we don't have a target
+ * Only turn it PI/8 radians if we have a lock
  */
-
 public class WideLock extends Component{
 
   private static final double MAX_RADAR_TURN = Math.PI / 4;
 
-  private Beck robot;
+  private AdvancedRobot robot;
   private long lastScanTime;
   ScannedRobotEvent target;
 
-  public WideLock(Beck robot) {
+  public WideLock(AdvancedRobot robot) {
       this.robot = robot;
   }
 
-//-- core ----------------------------------------------------------------------------------------------------
+//-- Core ----------------------------------------------------------------------------------------------------
 
+  /**
+   * Called by parent Robot every tick
+   */
   public void execute() {
     double radarTurn, radarDirection;
 
@@ -48,13 +48,20 @@ public class WideLock extends Component{
     robot.setTurnRadarRightRadians(radarTurn);
   }
 
+  /**
+   * Called by parent Robot at the start of a round
+   */
   public void init() {
     this.target = null;
-    //this.direction = 1;
   }
 
-//-- events --------------------------------------------------------------------------------------------------
+//-- Event Handling ------------------------------------------------------------------------------------------
 
+  /**
+   * Update target and record scan time
+   *
+   * @param e Scanned robot
+   */
   public void onScannedRobot(ScannedRobotEvent e) {
     target = e;
     lastScanTime = robot.getTime();
